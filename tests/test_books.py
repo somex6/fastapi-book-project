5cebef371385f4e5fb16dc7ad3d5740bdbcf4f72
somex6 <iwekasomex61@gmail.com>
2025-02-12 01:01:21 +0100

    fix code

--- tests/test_books.py
+++ tests/test_books.py
@@ -2,13 +2,13 @@ from tests import client
 
 
 def test_get_all_books():
-    response = client.get("/api/v1/books/")
+    response = client.get("/books/")
     assert response.status_code == 200
     assert len(response.json()) == 3
 
 
 def test_get_single_book():
-    response = client.get("/api/v1/books/1")
+    response = client.get("/books/1")
     assert response.status_code == 200
     data = response.json()
     assert data["title"] == "The Hobbit"
@@ -23,7 +23,7 @@ def test_create_book():
         "publication_year": 1997,
         "genre": "Fantasy",
     }
-    response = client.post("/api/v1/books/", json=new_book)
+    response = client.post("/books/", json=new_book)
     assert response.status_code == 201
     data = response.json()
     assert data["id"] == 4
@@ -38,15 +38,15 @@ def test_update_book():
         "publication_year": 1937,
         "genre": "Fantasy",
     }
-    response = client.put("/api/v1/books/1", json=updated_book)
+    response = client.put("/books/1", json=updated_book)
     assert response.status_code == 200
     data = response.json()
     assert data["title"] == "The Hobbit: An Unexpected Journey"
 
 
 def test_delete_book():
-    response = client.delete("/api/v1/books/3")
+    response = client.delete("/books/3")
     assert response.status_code == 204
 
-    response = client.get("/api/v1/books/3")
+    response = client.get("/books/3")
     assert response.status_code == 404
